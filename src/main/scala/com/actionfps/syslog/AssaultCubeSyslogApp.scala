a3ec24ec427e5abf95180eda4712669471e1d1c6
ScalaWilliam <william@scalawilliam.com>
2019-03-16 20:59:57 +0100

    Nicety Nicely :-)

--- src/main/scala/com/actionfps/syslog/AssaultCubeSyslogApp.scala
+++ src/main/scala/com/actionfps/syslog/AssaultCubeSyslogApp.scala
@@ -1,10 +1,13 @@
 package com.actionfps.syslog
 
 import java.net.{InetSocketAddress, URI}
+import java.nio.file._
+
 import cats.effect.{ContextShift, IO}
 import com.typesafe.scalalogging.StrictLogging
 import fs2._
 import fs2.io.udp.{AsynchronousSocketGroup, Socket}
+
 import scala.concurrent.ExecutionContext
 
 object AssaultCubeSyslogApp extends App with StrictLogging {
@@ -12,15 +15,25 @@ object AssaultCubeSyslogApp extends App with StrictLogging {
   private val bindUri = new URI(args(0))
   private implicit val grp: AsynchronousSocketGroup = AsynchronousSocketGroup()
   require(bindUri.getScheme == "udp", "Only UDP supported now")
+  private val targetPath = Paths.get(args(1))
+  require(Files.isWritable(targetPath.getParent), s"Make sure we can write to $targetPath")
   private val socketResource = Socket[IO](
     address = new InetSocketAddress(bindUri.getHost, bindUri.getPort)
   )
+
+  def appendLine(path: Path)(line: String): IO[Unit] = {
+    IO.delay {
+      Files.write(path, (line + "\n").getBytes(), StandardOpenOption.APPEND)
+    }
+  }
+
   fs2.Stream
     .resource(socketResource)
     .flatMap(_.reads())
     .through(packetsToAcServerMessages)
     .through(filterDefiniteAcServerMessages)
-    .evalMap(o => IO.delay(println(o)))
+    .map(_.toLine)
+    .evalMap(appendLine(targetPath))
     .compile
     .drain
     .unsafeRunSync()
